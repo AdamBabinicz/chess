be48b724ad4b59ae97873c0d839f0e638dabb473
babinicz <babinicz@onet.pl>
2025-02-06 17:01:47 +0100

    home

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Typography, Avatar, Grid, Box } from "@material-ui/core";
 import Typed from "react-typed";
-import avatar from "../images/6.jpg";
+import avatar from "../images/vik.avif";
 
 // CSS STYLES
 const useStyles = makeStyles((theme) => ({
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -51,7 +51,7 @@ const useStyles = makeStyles((theme) => ({
 const menuItems = [
   {
     listIcon: <Home />,
-    listText: "Home",
+    listText: "Start",
     listPath: "/",
   },
   {
